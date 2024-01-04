bad59aa707524b75f15e09e7665acba007e7279a
omid-hdr <omidheydari2004@gmail.com>
2024-01-04 22:41:51 +0330

    debug refactor

--- main.cpp
+++ main.cpp
@@ -2,32 +2,42 @@
 
 #define MAX_INPUT 200000
 #define MOD 1000000007
-long long rightFens[MAX_INPUT]; // right inversion
-long long leftFens[MAX_INPUT]; // left inversion
-long long amount[MAX_INPUT]; // 0 or 1
-long long indexes[MAX_INPUT];
+#define ll long long
+ll rightFens[MAX_INPUT]; // right inversion
+ll leftFens[MAX_INPUT]; // left inversion
+ll amount[MAX_INPUT]; // 0 or 1
+ll indexes[MAX_INPUT];
 int n;
 
-long long getSum(long long *BITree, int index)
-{
-    int sum = 0;
+ll getSum(const ll BITree[], int index) {
+    ll sum = 0;
     index = index + 1;
-    while (index>0)
-    {
+    while (index > 0) {
         sum += BITree[index];
         index -= index & (-index);
     }
     return sum;
 }
 
-void updateBIT(long long BITree[], int index, long long val)
-{
+void updateBIT(ll BITree[], int index, ll val) {
     index = index + 1;
-    while (index <= n)
-    {
+    while (index <= n) {
         BITree[index] += val;
         index += index & (-index);
     }
+}
+
+void update(int a, ll int leftAmount, ll int rightAmount) {
+    updateBIT(amount, a, 1);
+    updateBIT(leftFens, a, leftAmount);
+    updateBIT(rightFens, a, rightAmount);
+}
+
+ll calculate(int a, ll int leftAmount, ll int rightAmount) {
+    ll leftInverse = ((leftAmount * (leftAmount - 1)) / 2 - getSum(leftFens, a - 1)) % MOD;
+    ll rightInverse = ((rightAmount * (rightAmount - 1)) / 2 -
+                       (getSum(rightFens, n) - getSum(rightFens, a))) % MOD;
+    return (leftInverse * rightInverse) % MOD;
 }
 
 using namespace std;
@@ -42,28 +52,24 @@ int main() {
     cin >> n;
     int a;
 
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 0; i < n; ++i) {
         cin >> a;
-        indexes[i] = a;
+        indexes[a] = i;
     }
 
-    long long leftInverse, rightInverse, // amount of inversion
-    leftAmount, rightAmount, sum = 0; // amount of bigger or less number
+    ll leftAmount, rightAmount, sum = 0; // amount of bigger or less number
+
     for (int i = n; i > 0; --i) {
-        a = (int)indexes[i];
-        leftAmount = getSum(amount, a);
+        a = (int) indexes[i];
+        leftAmount = getSum(amount, a - 1);
         rightAmount = getSum(amount, n) - getSum(amount, a);
-        updateBIT(amount, a, 1);
-        updateBIT(leftFens,  a, leftAmount);
-        updateBIT(rightFens, a, rightAmount);
-//        if (leftAmount < 2) continue;
-//        if (rightAmount < 2) continue;
-        leftInverse = ((leftAmount * (leftAmount - 1)) / 2 - getSum(leftFens, a - 1)) % MOD;
-        rightInverse = ((rightAmount * (rightAmount - 1)) / 2 -
-                (getSum(rightFens, n) - getSum(rightFens, a))) % MOD;
-        sum += (leftInverse * rightInverse) % MOD;
+
+        update(a, leftAmount, rightAmount);
+
+        if (leftAmount < 2) continue;
+        if (rightAmount < 2) continue;
+        sum += calculate(a, leftAmount, rightAmount);
         sum %= MOD;
-        cout << a << " l:" << leftAmount << " r:" << rightAmount << "   l:" << leftInverse << " r:" << rightInverse << "  " << sum << "\n";
     }
     cout << sum;
 
